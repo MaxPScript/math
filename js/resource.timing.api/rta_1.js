ea65820215206a26ddd3a81460dead4f6a16c44e
Maxim <maxtab1@mail.ru>
2026-02-04 03:01:34 +0300

    create rta_1.js for basic work with resource timing api

---
+++ js/resource.timing.api/rta_1.js
@@ -0,0 +1,18 @@
+const resources = performance.getEntriesByType("resource");
+// console.log(resources);
+resources.forEach((resource) => {
+	// console.log(resource.name);
+});
+// Find the total page load span to calculate percentages for the bars
+const earliestStart = Math.min(...resources.map((r) => r.startTime));
+const latestEnd = Math.max(...resources.map((r) => r.responseEnd));
+const totalTime = latestEnd - earliestStart;
+
+const chartData = resources.map((entry) => ({
+	name: entry.name.split("/").pop() || entry.name, // Just the filename
+	startPct: ((entry.startTime - earliestStart) / totalTime) * 100,
+	durationPct: (entry.duration / totalTime) * 100,
+	type: entry.initiatorType,
+	fullEntry: entry,
+}));
+console.log(chartData);
