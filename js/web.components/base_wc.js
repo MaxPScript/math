a9c4faeb2f043d39d882c4febf26eb680020155f
Maxim <maxtab1@mail.ru>
2026-02-06 05:14:47 +0300

    Regular

--- js/web.components/base_wc.js
+++ js/web.components/base_wc.js
@@ -1,6 +1,7 @@
+// imgSrc: "../../assets/img/layout_1536_1024.webp",
 const MEDIA = {
 	imgRefEl: "#papyrus_id",
-	imgSrc: "../../assets/img/layout_1536_1024.webp",
+	imgSrc: "../assets/img/layout_1536_1024.webp",
 	imgW: 1536,
 	imgH: 1024,
 	soundSrc: "../../assets/audio/hover_6.wav",
