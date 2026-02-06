be7f2e2770ca8ada17d2c8f6b2ca6cd1a4666bb0
Maxim <maxtab1@mail.ru>
2026-02-06 05:30:27 +0300

    Regular

--- js/web.components/base_wc.js
+++ js/web.components/base_wc.js
@@ -1,10 +1,11 @@
 // imgSrc: "../../assets/img/layout_1536_1024.webp",
+// imgSrc: "../assets/img/layout_1536_1024.webp",
 const MEDIA = {
 	imgRefEl: "#papyrus_id",
-	imgSrc: "../assets/img/layout_1536_1024.webp",
+	imgSrc: "assets/img/layout_1536_1024.webp",
 	imgW: 1536,
 	imgH: 1024,
-	soundSrc: "../../assets/audio/hover_6.wav",
+	soundSrc: "assets/audio/hover_6.wav",
 };
 //
 export class BaseWC extends HTMLElement {
